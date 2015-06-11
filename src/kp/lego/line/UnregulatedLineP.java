6c5e247b69f450d6bdd58aa8c141bcff182f3a4a
pete <kapeterson@gmail.com>
2015-06-11 15:59:30 -0500

    added a simple P controller:

--- src/kp/lego/line/UnregulatedLineP.java
+++ src/kp/lego/line/UnregulatedLineP.java
@@ -7,7 +7,7 @@ import lejos.hardware.sensor.EV3ColorSensor;
 import lejos.robotics.SampleProvider;
 import lejos.utility.Delay;
 
-public class UnregulatedLineFollow {
+public class UnregulatedLineP {
 
 	/**
 	 * @param args
@@ -33,70 +33,46 @@ public class UnregulatedLineFollow {
 
 		int basePower = 100;
 		int tpower = 90;
-		float coefficient = .6f;
+		float coefficient = 1.0f;
 		
-		//lMotor.setPower(basePower);
-		//rMotor.setPower(basePower);
-		
-		//lMotor.forward();
-		//rMotor.forward();
-		System.out.println("We go");
-		
-		/*
-		Delay.msDelay(2000);
-		lMotor.setPower((int)(basePower*.75f));
-		Delay.msDelay(1000);
-		rMotor.setPower((int)(basePower*.75f));
-		Delay.msDelay(1000);
-		
-		
-		lMotor.setPower(basePower);
-		Delay.msDelay(1000);
-		rMotor.setPower(basePower);
-		*/
+
 		
 		
 		// sensor should sit on left side of line
 		int maxp = 0;
 		int minp = 1000;
 		
-		for ( int i = 0; i < 2500; i++){
+		for ( int i = 0; i < 6000; i++){
 			float[] sample = new float[1];
 			lightProvider.fetchSample(sample, 0);
 			float thissample =  sample[0];
 			thissample *= 100;
-			
-			int lp = (int)(coefficient*(whiteval -thissample));
-			int rp = (int)(coefficient*(thissample - blackval));
 
-			if ( lp < minp)
-				minp = lp;
-			if ( rp < minp)
-				minp = rp;
 			
-			if ( lp > maxp)
-				maxp = lp;
-			if ( rp > maxp)
-				maxp = rp;
-			lMotor.setPower(lp);
-			rMotor.setPower(rp);
-			//System.out.println("rp = " + rp + " lp = " + lp);
-			int correction = (int)(whiteval - thissample);
-			/*
 			int correction = (int)(coefficient*(thissample - mid));
 			if ( correction < 0 ){
-				System.out.println("reduce power to  mother to go to left to white");
-				lMotor.setPower(basePower - Math.abs(correction));
+				//System.out.println("reduce power to  left mother to go to left to white");
+				//lMotor.setPower((int)mid - Math.abs(correction));
+				//rMotor.setPower((int)mid+Math.abs(correction));
+				
+				lMotor.setPower(basePower-Math.abs(correction));
 				rMotor.setPower(basePower);
+				
+
 			} else {
-				System.out.println("reduce power to right motor to go right to black");
+				//System.out.println("reduce power to right motor to go right to black");
+				//lMotor.setPower((int)mid+Math.abs(correction));
+				//rMotor.setPower((int)mid- Math.abs(correction));
 				lMotor.setPower(basePower);
-				rMotor.setPower(basePower- Math.abs(correction));
+				rMotor.setPower(basePower-Math.abs(correction));
+				
+				if ( mid + Math.abs(correction) > maxp)
+					maxp = (int)mid + Math.abs(correction);
 			}
-			*/
+			
 			//System.out.println("sample : " + thissample + " correction = " +correction +  " mid = " + mid);
 			//System.out.println("*********");
-			Delay.msDelay(20);
+			//Delay.msDelay(20);
 		}
 		lMotor.flt();
 		rMotor.flt();
