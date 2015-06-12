af997d584f6de86f05e33a07f0c0ba9fec578e14
pete <kapeterson@gmail.com>
2015-06-12 14:52:12 -0500

    added working p controller

--- src/kp/lego/line/UnregulatedLineP.java
+++ src/kp/lego/line/UnregulatedLineP.java
@@ -14,7 +14,7 @@ public class UnregulatedLineP {
 	 */
 	static UnregulatedMotor lMotor;
 	static UnregulatedMotor rMotor;
-	public static float whiteval = 95.0f;
+	public static float whiteval = 90.0f;
 	public static float blackval = 8.0f;
 	public static float mid = (( whiteval - blackval ) / 2 )+ (blackval);
 	public static void main(String[] args) {
@@ -28,56 +28,69 @@ public class UnregulatedLineP {
 		
 		
 		try {
-		lMotor = new UnregulatedMotor(LocalEV3.get().getPort("A"));
-		rMotor = new UnregulatedMotor(LocalEV3.get().getPort("B"));
+			lMotor = new UnregulatedMotor(LocalEV3.get().getPort("A"));
+			rMotor = new UnregulatedMotor(LocalEV3.get().getPort("B"));
 
-		int basePower = 100;
-		int tpower = 90;
-		float coefficient = 1.0f;
+			int basePower = 60;
+			float coefficient = 1.0f;
 		
 
 		
 		
-		// sensor should sit on left side of line
-		int maxp = 0;
-		int minp = 1000;
+			// sensor should sit on left side of line
+			int maxp = 0;
+			int	 minp = 1000;
+			float maxsample = 0.0f;
+			float minsample = 1.0f;
 		
-		for ( int i = 0; i < 6000; i++){
-			float[] sample = new float[1];
-			lightProvider.fetchSample(sample, 0);
-			float thissample =  sample[0];
-			thissample *= 100;
-
+			for ( int i = 0; i < 10000; i++){
+				float[] sample = new float[1];
+				lightProvider.fetchSample(sample, 0);
+				float thissample =  sample[0];
+				if (thissample < minsample)
+					minsample= thissample;
 			
-			int correction = (int)(coefficient*(thissample - mid));
-			if ( correction < 0 ){
-				//System.out.println("reduce power to  left mother to go to left to white");
-				//lMotor.setPower((int)mid - Math.abs(correction));
-				//rMotor.setPower((int)mid+Math.abs(correction));
+				if ( thissample > maxsample)
+					maxsample = thissample;
+			
+				thissample *= 100;
+
+				// PLace on left side
+			
+				int correction = (int)(coefficient*(thissample - mid));
+				if ( correction < 0 ){
+
 				
-				lMotor.setPower(basePower-Math.abs(correction));
-				rMotor.setPower(basePower);
+				
+					if ( basePower - Math.abs(correction) < minp)
+						minp = basePower - Math.abs(correction);
+				
+					lMotor.setPower(basePower-Math.abs(correction));
+					rMotor.setPower(basePower);
 				
 
-			} else {
-				//System.out.println("reduce power to right motor to go right to black");
-				//lMotor.setPower((int)mid+Math.abs(correction));
-				//rMotor.setPower((int)mid- Math.abs(correction));
-				lMotor.setPower(basePower);
-				rMotor.setPower(basePower-Math.abs(correction));
+				} else {
+
+					lMotor.setPower(basePower);
+					rMotor.setPower(basePower-Math.abs(correction));
 				
-				if ( mid + Math.abs(correction) > maxp)
-					maxp = (int)mid + Math.abs(correction);
+					if ( basePower - Math.abs(correction) < minp)
+						minp = basePower - Math.abs(correction);
+								
+					if ( mid + Math.abs(correction) > maxp)
+						maxp = (int)mid + Math.abs(correction);
+				}	
+			
+
+				Delay.msDelay(20);
 			}
-			
-			//System.out.println("sample : " + thissample + " correction = " +correction +  " mid = " + mid);
-			//System.out.println("*********");
-			//Delay.msDelay(20);
-		}
-		lMotor.flt();
-		rMotor.flt();
-		System.out.println("maxp = " + maxp);
-		System.out.println("minp = " + minp);
+			lMotor.flt();
+			rMotor.flt();
+			System.out.println("maxp = " + maxp);
+			System.out.println("minp = " + minp);
+			System.out.println("max sample = " + maxsample);
+			System.out.println("min sample = " + minsample);
+		
 		} catch (Exception err){
 			lMotor.close();
 			rMotor.close();
